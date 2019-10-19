2225ea5fdc709ce78222dc0ae38f5f5b2a0433ae
Sergey Shkarupa <s.shkarupa@gmail.com>
2019-10-19 13:39:03 +0200

    add authorization

--- lib/islands_interface_web/channels/game_channel.ex
+++ lib/islands_interface_web/channels/game_channel.ex
@@ -5,8 +5,12 @@ defmodule IslandsInterfaceWeb.GameChannel do
   alias IslandsInterfaceWeb.Presence
 
   def join("game:" <> _player, %{"screen_name" => screen_name}, socket) do
-    send(self(), {:after_join, screen_name})
-    {:ok, socket}
+    if authorized?(socket, screen_name) do
+      send(self(), {:after_join, screen_name})
+      {:ok, socket}
+    else
+      {:error, %{reason: "unauthorized"}}
+    end
   end
 
   def handle_info({:after_join, screen_name}, socket) do
@@ -86,4 +90,21 @@ defmodule IslandsInterfaceWeb.GameChannel do
   end
 
   defp via("game:" <> player), do: Game.via_tuple(player)
+
+  defp authorized?(socket, screen_name) do
+    number_of_players(socket) < 2 && !existing_payer?(socket, screen_name)
+  end
+
+  defp number_of_players(socket) do
+    socket
+    |> Presence.list()
+    |> Map.keys()
+    |> length()
+  end
+
+  defp existing_payer?(socket, screen_name) do
+    socket
+    |> Presence.list()
+    |> Map.has_key?(screen_name)
+  end
 end
